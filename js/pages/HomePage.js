b668d7400478b81f05ae78ed1df7e43f296b034d
Hawk Song <savokisong@gmail.com>
2020-12-14 21:56:59 -0800

    bottom tab icon

--- js/pages/HomePage.js
+++ js/pages/HomePage.js
@@ -6,32 +6,65 @@ import TrendingPage from './TrendingPage';
 import FavoritePage from './FavoritePage';
 import MyPage from './MyPage';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Tab = createBottomTabNavigator();
 export default class HomePage extends Component {
   render() {
     return (
       <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            // You can return any component that you like here!
-            return (
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen
+          name="Hottest"
+          component={PopularPage}
+          options={{
+            tabBarIcon: ({color, size}) => (
               <MaterialIcons
                 name={'whatshot'}
                 size={26}
                 style={{color: color}}
               />
-            );
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
-        <Tab.Screen name="Hottest" component={PopularPage} />
-        <Tab.Screen name="Trending" component={TrendingPage} />
-        <Tab.Screen name="Favorite" component={FavoritePage} />
-        <Tab.Screen name="My" component={MyPage} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Trending"
+          component={TrendingPage}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Ionicons
+                name={'md-trending-up'}
+                size={26}
+                style={{color: color}}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorite"
+          component={FavoritePage}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialIcons
+                name={'favorite'}
+                size={26}
+                style={{color: color}}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="My"
+          component={MyPage}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Entypo name={'user'} size={26} style={{color: color}} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     );
   }
