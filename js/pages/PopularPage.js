8f4d44b6bf95939ba3845b044e14e820fc29d642
Hawk Song <savokisong@gmail.com>
2021-01-02 21:02:54 -0800

    dynamic top tab

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -5,32 +5,35 @@ import {StyleSheet, Text, View} from 'react-native';
 const Tab = createMaterialTopTabNavigator();
 const PopularPage = (props) => {
   const tabNames = ['Java', 'Android', 'iOS', 'React', 'React Native', 'PHP'];
-  const tabs = {};
-  tabNames.forEach((item, index) => {
-    tabs[`tab${index}`] = {
-      screen: (props) => <PopularPage {...props} tabLabel={item} />,
-    };
-  });
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Popular" component={PopularTab} />
-      <Tab.Screen name="Popular2" component={PopularTab2} />
+    <Tab.Navigator
+      tabBarOptions={{
+        tabStyle: styles.tabStyle,
+        upperCAseLabel: false,
+        scrollEnabled: true,
+        style: {
+          backgroundColor: '#a67',
+        },
+        indicatorStyle: styles.indicatorStyle,
+        labelStyle: styles.labelStyle,
+      }}>
+      {tabNames.map((tabName, index) => {
+        return (
+          <Tab.Screen
+            name={tabName}
+            component={PopularTab}
+            key={`${tabName}${index}`}
+          />
+        );
+      })}
     </Tab.Navigator>
   );
 };
 
-const PopularTab = (props) => {
+const PopularTab = ({route}) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>PopularTab</Text>
-    </View>
-  );
-};
-
-const PopularTab2 = (props) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>PopularTab2</Text>
+      <Text style={styles.welcome}>{route.name}</Text>
     </View>
   );
 };
@@ -44,6 +47,18 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  tabStyle: {
+    minWidth: 50,
+  },
+  indicatorStyle: {
+    height: 2,
+    backgroundColor: 'white',
+  },
+  labelStyle: {
+    fontSize: 13,
+    marginTop: 6,
+    marginBottom: 6,
   },
 });
 
